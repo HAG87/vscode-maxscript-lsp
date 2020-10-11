e0eb6c9c028a6987c7594033da11bcc82f313a44
Atelier Bump <ariel.g87@gmail.com>
2020-10-10 21:07:04 -0500

    Webpack somewhat working!

--- shared.webpack.config.js
+++ shared.webpack.config.js
@@ -5,11 +5,13 @@
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
-
+// /** @typedef {import('tsconfig-paths-webpack-plugin').TsconfigPathsPlugin} TsconfigPaths **/
 'use strict';
 
 const path = require('path');
 const mergeOptions = require('merge-options');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+// import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 // const nodeExternals = require('webpack-node-externals');
 
 module.exports = function withDefaults(/**@type WebpackConfig*/extConfig) {
@@ -18,35 +20,53 @@ module.exports = function withDefaults(/**@type WebpackConfig*/extConfig) {
 	let defaultConfig = {
 		mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 		target: 'node', // extensions run in a node context
+		// /*
 		node: {
 			__dirname: false // leave the __dirname-behaviour intact
 		},
+		// */
 		resolve: {
 			mainFields: ['module', 'main'],
-			extensions: ['.ts', '.js'],
-			alias: {
-				// 'node_modules': path.join(__dirname, '/node_modules/'),
-			}
+			extensions: ['.ts', '.tsx', '.js', '.jsx'],
+			modules: [path.resolve(__dirname, 'src'), 'node_modules', path.resolve(__dirname)],
+			/*
+			plugins: [
+				//@ts-expect-error
+				new TsconfigPathsPlugin({
+					extensions: ['.ts', '.tsx', '.js', '.jsx'],
+					mainFields: ['module', 'main'],
+				})]
+				*/
 		},
 		module: {
-			rules: [{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: [{
-					// configure TypeScript loader:
-					// * enable sources maps for end-to-end source maps
-					loader: 'ts-loader',
-					options: {
-						compilerOptions: {
-							'sourceMap': true,
+			rules: [
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					use: [{
+						// loader: require.resolve('ts-loader'),
+						loader: 'ts-loader',
+						// configure TypeScript loader:
+						// * enable sources maps for end-to-end source maps
+						options: {
+							// context: __dirname,
+							// configFile: require.resolve('tsconfig.base'),
+							compilerOptions: {
+								'sourceMap': true,
+								// 'onlyCompileBundledFiles': true,
+								'projectReferences': true
+							}
 						}
-					}
-				}]
-			},
-			{
-				test: /node_modules/,
-				use: 'umd-compat-loader'
-			}]
+					}]
+				},
+				/*
+				{
+					test: /\.js$/,
+					type: 'javascript/esm',
+					include: /node_modules/
+				},
+				*/
+			]
 		},
 		externals:
 		{
@@ -54,9 +74,9 @@ module.exports = function withDefaults(/**@type WebpackConfig*/extConfig) {
 		},
 		output: {
 			filename: '[name].js',
-			chunkFilename: '[name].js',
 			path: path.join(extConfig.context, 'out'),
-			libraryTarget: 'commonjs2',
+			libraryTarget: 'commonjs',
+			// chunkFilename: '[name].js',
 		},
 		// yes, really source maps
 		devtool: 'source-map'
