99c45c2055c07792adda07b58fd28c9024317c0e
Atelier Bump <ariel.g87@gmail.com>
2020-10-12 20:24:56 -0500

    Webpack working state

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,9 +3,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 //@ts-check
-
 'use strict';
 
 const withDefaults = require('../shared.webpack.config');
@@ -20,42 +18,48 @@ module.exports = withDefaults({
 		filename: '[name].js',
 		path: path.join(__dirname, 'out')
 	},
+	// resolve: {
+	// 	roots: [path.resolve('./src')]
+	// },
 	/*
-	externals: {
-		'ast-get-object': 'commonjs ast-get-object',
-		'ast-get-values-by-key': 'commonjs ast-get-values-by-key',
-		'ast-monkey-traverse': 'commonjs ast-monkey-traverse',
-		'ast-compare': 'commonjs ast-compare',
-		'object-path' : 'commonjs object-path',
-		// 'vscode-languageserver' : 'commonjs vscode-languageserver',
-		// 'vscode-languageserver-textdocument': 'commonjs vscode-languageserver-textdocument',
-		// 'vscode-uri': 'commonjs vscode-uri',
-		'lodash': 'commonjs lodash',
-		'matcher': 'commonjs matcher',
-		'moo': 'commonjs moo',
-		'nearley': 'commonjs nearley',
+	module: {
+		rules: [
+			{
+				// test: [/\.tsx?$/, /\.js$/ ],
+				// test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'ts-loader',
+					options: {
+						compilerOptions: {
+							// 'allowJs': false
+							// 'noImplicitAny': true
+						},
+						// projectReferences: true,
+					}
+				}]
+			},
+		]
 	},
-	//*/
-	/*
+	// */
+	// /*
 	optimization: {
 		runtimeChunk: 'single',
 		splitChunks: {
-			maxInitialRequests: Infinity,
-			minSize: 0,
 			cacheGroups: {
-				vendor: {
+				defaultVendors: {
 					chunks: 'all',
-					minChunks:1,
-					enforce: true
+					enforce: true,
 					test: /[\\/]node_modules[\\/](!vscode-languageserver)(!vscode-languageserver-textdocument)(!vscode-uri)[\\/]/,
-					name: 'vendor',
-					name(module) {
-						// get the name. E.g. node_modules/packageName/not/this/part.js
-						// or node_modules/packageName
-						const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-						// npm package names are URL-safe, but some servers don't like @ symbols
-						return `${packageName.replace('@', '')}`;
-					},
+					filename: 'vendors.js'
+					// name: 'vendor',
+					// name(module) {
+					// 	// get the name. E.g. node_modules/packageName/not/this/part.js
+					// 	// or node_modules/packageName
+					// 	const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+					// 	// npm package names are URL-safe, but some servers don't like @ symbols
+					// 	return `${packageName.replace('@', '')}`;
+					// },
 				},
 			},
 		},
--- shared.webpack.config.js
+++ shared.webpack.config.js
@@ -5,38 +5,31 @@
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
-// /** @typedef {import('tsconfig-paths-webpack-plugin').TsconfigPathsPlugin} TsconfigPaths **/
 'use strict';
 
 const path = require('path');
 const mergeOptions = require('merge-options');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 // import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
-// const nodeExternals = require('webpack-node-externals');
 
 module.exports = function withDefaults(/**@type WebpackConfig*/extConfig) {
-
 	/** @type WebpackConfig */
 	let defaultConfig = {
 		mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 		target: 'node', // extensions run in a node context
-		// /*
 		node: {
 			__dirname: false // leave the __dirname-behaviour intact
 		},
-		// */
 		resolve: {
 			mainFields: ['module', 'main'],
 			extensions: ['.ts', '.tsx', '.js', '.jsx'],
-			modules: [path.resolve(__dirname, 'src'), 'node_modules', path.resolve(__dirname)],
-			/*
+			modules: ['node_modules', path.resolve(__dirname)],
 			plugins: [
 				//@ts-expect-error
 				new TsconfigPathsPlugin({
 					extensions: ['.ts', '.tsx', '.js', '.jsx'],
 					mainFields: ['module', 'main'],
 				})]
-				*/
 		},
 		module: {
 			rules: [
@@ -44,28 +37,20 @@ module.exports = function withDefaults(/**@type WebpackConfig*/extConfig) {
 					test: /\.tsx?$/,
 					exclude: /node_modules/,
 					use: [{
-						// loader: require.resolve('ts-loader'),
 						loader: 'ts-loader',
-						// configure TypeScript loader:
-						// * enable sources maps for end-to-end source maps
 						options: {
-							// context: __dirname,
-							// configFile: require.resolve('tsconfig.base'),
-							compilerOptions: {
-								'sourceMap': true,
-								// 'onlyCompileBundledFiles': true,
-								'projectReferences': true
-							}
+							// configure TypeScript loader:
+							// * enable sources maps for end-to-end source maps
+							// compilerOptions: {
+							// 'sourceMap': true,
+							// 'allowJs': false,
+							// },
+							projectReferences: true,
+							// transpileOnly: false,
+							// onlyCompileBundledFiles: true,
 						}
 					}]
 				},
-				/*
-				{
-					test: /\.js$/,
-					type: 'javascript/esm',
-					include: /node_modules/
-				},
-				*/
 			]
 		},
 		externals:
@@ -78,10 +63,8 @@ module.exports = function withDefaults(/**@type WebpackConfig*/extConfig) {
 			libraryTarget: 'commonjs',
 			// chunkFilename: '[name].js',
 		},
-		// yes, really source maps
 		devtool: 'source-map'
 	};
-
 	// @ts-ignore
 	return mergeOptions(defaultConfig, extConfig);
 };
