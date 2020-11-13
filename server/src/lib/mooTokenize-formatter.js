759f6a658223a3a49accb3e8d72ddb9eed7ad838
Atelier Bump <ariel.g87@gmail.com>
2020-11-13 08:54:50 -0600

    Formatter tokens simplification

--- server/src/lib/mooTokenize-formatter.js
+++ server/src/lib/mooTokenize-formatter.js
@@ -1,4 +1,6 @@
-// moo tokenizer
+/**	
+ * Simplified tokenizer for code formatting
+ */
 import { keywords, compile } from 'moo';
 //-----------------------------------------------------------------------------------
 // CASE INSENSITIVE FOR KEYWORKDS
@@ -8,96 +10,11 @@ const caseInsensitiveKeywords = map => {
 };
 //-----------------------------------------------------------------------------------
 // KEYWORDS
-const UIcontrols = [
-	'angle', 'bitmap', 'button',
-	'checkbox', 'checkbutton', 'colorpicker',
-	'combobox', 'curvecontrol', 'dropdownlist',
-	'edittext', 'groupbox', 'hyperlink',
-	'imgtag', 'label', 'listbox',
-	'mapbutton', 'materialbutton', 'multilistbox',
-	'pickbutton', 'popupmenu', 'progressbar',
-	'radiobuttons', 'slider', 'spinner',
-	'subrollout', 'timer', 'dotnetcontrol'
-];
-const kwContext = [
-	'animate',
-	'redraw',
-	'quiet',
-	'printallelements',
-	'mxscallstackcaptureenabled',
-	'dontrepeatmessages',
-	'macrorecorderemitterenabled'
-];
-const kwObjectSet = [
-	'objects', 'geometry', 'lights', 'cameras', 'helpers',
-	'shapes', 'systems', 'spacewarps', 'selection'
-];
-const keywordsDB = {
-	'kw_about': 'about',
-	'kw_as': 'as',
-	'kw_at': 'at',
-	'kw_bool': ['true', 'false', 'off'],
-	'kw_by': 'by',
-	'kw_case': 'case',
-	'kw_catch': 'catch',
-	'kw_collect': 'collect',
-	'kw_compare': ['and', 'or'],
-	'kw_context': kwContext,
-	'kw_coordsys': 'coordsys',
-	'kw_defaultAction': 'defaultaction',
-	'kw_do': 'do',
-	'kw_else': 'else',
-	'kw_exit': 'exit',
-	'kw_for': 'for',
-	'kw_from': 'from',
-	'kw_function': ['function', 'fn'],
-	'kw_global': 'global',
-	'kw_group': 'group',
-	'kw_if': 'if',
-	'kw_in': 'in',
-	'kw_level': 'level',
-	'kw_local': 'local',
-	'kw_macroscript': 'macroscript',
-	'kw_mapped': 'mapped',
-	'kw_menuitem': 'menuitem',
-	'kw_not': 'not',
-	'kw_null': ['undefined', 'unsupplied', 'ok', 'silentvalue'],
-	'kw_objectset': kwObjectSet,
-	'kw_of': 'of',
-	'kw_on': 'on',
-	'kw_parameters': 'parameters',
-	'kw_persistent': 'persistent',
-	'kw_plugin': 'plugin',
-	'kw_rcmenu': 'rcmenu',
-	'kw_return': 'return',
-	'kw_rollout': 'rollout',
-	'kw_scope': ['private', 'public'],
-	'kw_separator': 'separator',
-	'kw_set': 'set',
-	'kw_struct': 'struct',
-	'kw_submenu': 'submenu',
-	'kw_then': 'then',
-	'kw_time': 'time',
-	'kw_to': 'to',
-	'kw_tool': 'tool',
-	'kw_try': 'try',
-	'kw_uicontrols': UIcontrols,
-	'kw_undo': 'undo',
-	'kw_utility': 'utility',
-	'kw_when': 'when',
-	'kw_where': 'where',
-	'kw_while': 'while',
-	'kw_with': 'with',
-	// 'kw_continue':    'continue',
-	// 'kw_dontcollect': 'dontcollect',
-	// 'kw_max':         'max',
-	// 'kw_redraw':      'redraw',
-	// 'kw_throw':       'throw',
-};
+import { keywordsDB } from './keywordsDB';
 //-----------------------------------------------------------------------------------
 // Moo Lexer
 var mxLexer = compile({
-	// the comments
+	// Comments
 	comment_SL: /--.*$/,
 	comment_BLK: { match: /\/\*(?:.|[\n\r])*?\*\//, lineBreaks: true, },
 	string: [
@@ -107,28 +24,22 @@ var mxLexer = compile({
 	// whitespace -  also matches line continuations
 	ws: { match: /(?:[ \t]+|(?:[\\][ \t\r\n]+))/, lineBreaks: true },
 	newline: { match: /(?:[\r\n]+)/, lineBreaks: true },
-
-	// path_name $mounstrosity*/_?
-	path: [
-		/[$](?:[A-Za-z0-9_*?/\\]|\.\.\.)+/,
-		'$'
-	],
+	// Identities
 	identity: [
+		/[$](?:[A-Za-z0-9_*?/\\]|\.\.\.)+/,
+		'$',
 		/'(?:\\['\\rn]|[^'\\\n])*?'/,
 		/#[A-Za-z0-9_]+\b/,
 		/#'[A-Za-z0-9_]+'/,
 		/~[A-Za-z0-9_]+~/,
-		/::[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
-		/[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=[ \t]*[:][^:])/,
-		/[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=\.)/,
-		/(?<=\.)[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
+		/::[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]+/,
 		{
-			match: /[&]?[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
+			match: /[&]?[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]+(?![:])/,
 			type: caseInsensitiveKeywords(keywordsDB)
 		}
 	],
-	param: ':',
-	// PARENS
+	param: /[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]+:/,
+	// Parens
 	arraydef: /#[ \t]*\(/,
 	bitarraydef: /#[ \t]*\{/,
 	emptyparens: /\([\s\t]*\)/,
@@ -139,7 +50,7 @@ var mxLexer = compile({
 	rbracket: ']',
 	lbrace: '{',
 	rbrace: '}',
-	
+	// Values
 	time: [
 		/(?:[-]?(?:[0-9]+\.)?[0-9]+[msft])+/,
 		/(?:[-]?(?:[0-9]+\.)[0-9]*[msft])+/,
@@ -154,9 +65,10 @@ var mxLexer = compile({
 		/(?:(?<!\.)[-]?\.[0-9]+(?:[eEdD][+-]?[0-9]+)?)/
 	],
 	// unary: {match: /(?<=[^\w)-])-(?![-\s])/},
+	// Operators
 	unaryminus: /-(?![-\s\t\r\n])/,
 	operator: ['+', '-', '*', '/', '^', '==', '!=', '>', '<', '>=', '<=', '=', '+=', '-=', '*=', '/='],
-	// DELIMITERS
+	// Delimiters
 	delimiter: '.',
 	sep: ',',
 	statement: ';',
