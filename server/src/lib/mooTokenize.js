dd03d951bd0c3e76f1b6e4ef4532c4ac77fc6105
Atelier Bump <ariel.g87@gmail.com>
2020-11-14 18:15:22 -0600

    tokenizer fix

--- server/src/lib/mooTokenize.js
+++ server/src/lib/mooTokenize.js
@@ -98,67 +98,76 @@ const keywordsDB = {
 // Moo Lexer
 var mxLexer = moo.compile({
 	// the comments
-	comment_SL:/--.*$/,
+	comment_SL: /--.*$/,
 	comment_BLK: { match: /\/\*(?:.|[\n\r])*?\*\//, lineBreaks: true, },
 	// strings
 	string: [
-		{ match: /@"(?:\\"|[^"])*?(?:"|\\")/, lineBreaks: true },
-		{ match: /"(?:\\["\\rntsx]|[^"])*?"/, lineBreaks: true },
+		{ match: /@"(?:\\"|[^"])*?(?:"|\\")/, lineBreaks: true},
+		{ match: /"(?:\\["\\rntsx]|[^"])*?"/, lineBreaks: true},
 		// { match: /"""[^]*?"""/, lineBreaks: true, value: x => x.slice(3, -3)},
 	],
 	// whitespace -  also matches line continuations
 	ws: { match: /(?:[ \t]+|(?:[\\][ \t\r\n]+))/, lineBreaks: true },
 	// newline: { match: /(?:[\r\n]|[\\]\s*[\r\n])+/, lineBreaks: true },
 	newline: { match: /(?:[\r\n]+)/, lineBreaks: true },
+	
+	// strings ~RESOURCE~
+	locale: /~[A-Za-z0-9_]+~/,
+	
 	// path_name $mounstrosity*/_?
 	path: [
 		/[$](?:[A-Za-z0-9_*?/\\]|\.\.\.)+/,
 		'$'
 	],
-	// strings ~RESOURCE~
-	locale: /~[A-Za-z0-9_]+~/,
-	// parameter <param_name>:
-	global_typed: /::[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
-	// params: { match: /[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=[ \t]*[:][^:])/ },
+
+	// ::global variable
+	global_typed: /::[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]+/,
 	// property <object>.<property>
 	// property: { match: /\.[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/, value: x => x.slice(1) },
-	// ::global variable
+	
 	// IDENTIFIERS
-	identity: [
-		/[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=[ \t]*[:][^:])/,
-		/[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=\.)/,
-		/(?<=\.)[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
+	// a mounstrosity
+	typed_iden: /'(?:\\['\\rn]|[^'\\\n])*?'/,
+	
+	// parameter <param_name>:
+	param_name: {
+		match:/[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=[:])/,
+		// value: x => x.slice(0, -1)
+	},
+	// params: { match: /[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*(?=[ \t]*[:][^:])/ },
+	param: ':',
+
+	identity: 
 		{
 			match: /[&]?[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
 			type: caseInsensitiveKeywords(keywordsDB)
-		}
-	],
-	// a mounstrosity
-	typed_iden: /'(?:\\['\\rn]|[^'\\\n])*?'/,
-
-	param: ':',
+		},
 	// array marker #(...) | #{...}
 	arraydef: /#[ \t]*\(/,
-	bitarraydef: /#[ \t]*{/,
+	bitarraydef: /#[ \t]*\{/,
 	// PARENS
 	lparen: '(',
 	rparen: ')',
+
 	// BRACKETS, BRACES...
 	lbracket: '[',
 	rbracket: ']',
 	lbrace: '{',
 	rbrace: '}',
+
 	// Operators.
 	comparison: ['==', '!=', '>', '<', '>=', '<='],
 	assign: ['=', '+=', '-=', '*=', '/='],
 	// unary: {match: /(?<=[^\w)-])-(?![-\s])/},
 	math: ['+', '-', '*', '/', '^'],
+
 	// time format
 	time: [
 		/(?:[-]?(?:[0-9]+[.])?[0-9]+[msft])+/,
 		/(?:[-]?(?:[0-9]+[.])[0-9]*[msft])+/,
 		/[0-9]+[:][0-9]+[.][0-9]*/
 	],
+
 	// number formats
 	bitrange: '..',
 	hex: /0[xX][0-9a-fA-F]+/,
@@ -168,16 +177,18 @@ var mxLexer = moo.compile({
 		/[-]?[0-9]+(?:[LP]|[eEdD][+-]?[0-9]+)?/, // 456 | 123e-5 | integers
 		/(?:(?<!\.)[-]?\.[0-9]+(?:[eEdD][+-]?[0-9]+)?)/ // -.789e-9
 	],
+
 	// #name literals .. should go higher??
 	name: [
 		/#[A-Za-z0-9_]+\b/,
 		/#'[A-Za-z0-9_]+'/
 	],
+
 	// DELIMITERS
 	delimiter: '.',
 	sep: ',',
-	// NEWLINES
 	statement: ';',
+
 	// [\$?`] COMPLETE WITH UNWANTED CHARS HERE THAT CAN BREAK THE TOKENIZER
 	error: [
 		{ match: /[¿¡!`´]/, error: true },
