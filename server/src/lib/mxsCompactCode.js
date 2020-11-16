afa27515456afe629c533def45cd79ab1794a967
Atelier Bump <ariel.g87@gmail.com>
2020-11-16 14:48:06 -0600

    Minifier working

--- server/src/lib/mxsCompactCode.js
+++ server/src/lib/mxsCompactCode.js
@@ -4,7 +4,7 @@
  * Check if value is node
  * @param {any} node CST node
  */
-const isNode = (node) =>  typeof node === 'object' && node != null;
+const isNode = (node) => typeof node === 'object' && node != null;
 /**
  * filter nodes by type property
  * @param {any} node CST node
@@ -21,21 +21,21 @@ function binary(left, right, op) {
 }
 //*/
 function binaryNode(node) {
-	let _left  = node.left || '';
+	let _left = node.left || '';
 	let _right = node.right || '';
-	let left   = `${_left}${spaceLR(_left, node.operator)}`;
-	let right  = `${spaceLR(node.operator, _right)}${_right}`;
+	let left = `${_left}${spaceLR(_left, node.operator)}`;
+	let right = `${spaceLR(node.operator, _right)}${_right}`;
 	return `${left}${node.operator}${right}`;
 }
 function exprTerm(exprArr) {
-	return (Array.isArray(exprArr) ? exprArr.join(';') : exprArr);
+	return (Array.isArray(exprArr) ? exprArr.filter(e => e != null || e !== '').join(';') : exprArr);
 }
 /**
  * Join string array
  * @param {string[] | undefined} arr
  */
 function joinStatements(arr) {
-	if (!arr || arr.length === 0) {return '';}
+	if (!arr || arr.length === 0) { return ''; }
 	return arr.reduce((acc, curr) => {
 		let term = curr || '';
 		return (acc + spaceLR(acc, term) + term);
@@ -47,7 +47,7 @@ function joinStatements(arr) {
  * @param {string} str2 Right string
  */
 function spaceLR(str1, str2) {
-	if (!str2 || !str1) {return '';}
+	if (!str2 || !str1) { return ''; }
 	return /[\w_$?-]$/gmi.test(str1) && /^(?:[\w_-]|::)/gmi.test(str2) ? ' ' : '';
 }
 /**
@@ -63,99 +63,99 @@ function spaceSE(str, end = true) {
 function spaceAlphaNum(str) {
 	let start = /^[\w]/gmi.test(str) ? ' ' : '';
 	let end = /[\w]$/gmi.test(str) ? ' ' : '';
-	return `${start}${str}${end}`; 
+	return `${start}${str}${end}`;
 
 }
 
 let tokensValue = {
-	assign       (node) { return node.value; },
-	comparison   (node) { return node.value; },
-	error        (node) { return node.text; }, // faulty tokens!
-	global_typed (node) { return node.text; },
-	hex          (node) { return node.text; },
-	identity     (node) { return node.text; },
-	
-	locale       (node) { return node.text; },
-	math         (node) { return node.value; },
-	name         (node) { return node.text; },
-	number       (node) { return node.text; },
-	param        (node) { return node.value; },
-	path         (node) { return node.text; },
-	property     (node) { return node.value; },
-	string       (node) { return node.text; },
-	time         (node) { return node.text; },
-	typed_iden   (node) { return node.text; },
-	
-	keyword      (node) { return node.text; },
-	
-	kw_about         (node) { return node.text; },
-	kw_as            (node) { return node.text; },
-	kw_at            (node) { return node.text; },
-	kw_bool          (node) { return node.text; },
-	kw_by            (node) { return node.text; },
-	kw_case          (node) { return node.text; },
-	kw_catch         (node) { return node.text; },
-	kw_collect       (node) { return node.text; },
-	kw_compare       (node) { return node.text; },
-	kw_context       (node) { return node.text; },
-	kw_coordsys      (node) { return node.text; },
-	kw_defaultAction (node) { return node.text; },
-	kw_do            (node) { return node.text; },
-	kw_else          (node) { return node.text; },
-	kw_exit          (node) { return node.text; },
-	kw_for           (node) { return node.text; },
-	kw_from          (node) { return node.text; },
-	kw_function      (node) { return node.text; },
-	kw_global        (node) { return node.text; },
-	kw_group         (node) { return node.text; },
-	kw_if            (node) { return node.text; },
-	kw_in            (node) { return node.text; },
-	kw_level         (node) { return node.text; },
-	kw_local         (node) { return node.text; },
-	kw_macroscript   (node) { return node.text; },
-	kw_mapped        (node) { return node.text; },
-	kw_menuitem      (node) { return node.text; },
-	kw_not           (node) { return node.text; },
-	kw_null          (node) { return node.text; },
-	kw_objectset     (node) { return node.text; },
-	kw_of            (node) { return node.text; },
-	kw_on            (node) { return node.text; },
-	kw_parameters    (node) { return node.text; },
-	kw_persistent    (node) { return node.text; },
-	kw_plugin        (node) { return node.text; },
-	kw_rcmenu        (node) { return node.text; },
-	kw_return        (node) { return node.text; },
-	kw_rollout       (node) { return node.text; },
-	kw_scope         (node) { return node.text; },
-	kw_separator     (node) { return node.text; },
-	kw_set           (node) { return node.text; },
-	kw_struct        (node) { return node.text; },
-	kw_submenu       (node) { return node.text; },
-	kw_then          (node) { return node.text; },
-	kw_time          (node) { return node.text; },
-	kw_to            (node) { return node.text; },
-	kw_tool          (node) { return node.text; },
-	kw_try           (node) { return node.text; },
-	kw_uicontrols    (node) { return node.text; },
-	kw_undo          (node) { return node.text; },
-	kw_utility       (node) { return node.text; },
-	kw_when          (node) { return node.text; },
-	kw_where         (node) { return node.text; },
-	kw_while         (node) { return node.text; },
-	kw_with          (node) { return node.text; },
+	assign(node) { return node.value; },
+	comparison(node) { return node.value; },
+	error(node) { return node.text; }, // faulty tokens!
+	global_typed(node) { return node.text; },
+	hex(node) { return node.text; },
+	identity(node) { return node.text; },
+
+	locale(node) { return node.text; },
+	math(node) { return node.value; },
+	name(node) { return node.text; },
+	number(node) { return node.text; },
+	param(node) { return node.value; },
+	path(node) { return node.text; },
+	property(node) { return node.value; },
+	string(node) { return node.text; },
+	time(node) { return node.text; },
+	typed_iden(node) { return node.text; },
+
+	keyword(node) { return node.text; },
+
+	kw_about(node) { return node.text; },
+	kw_as(node) { return node.text; },
+	kw_at(node) { return node.text; },
+	kw_bool(node) { return node.text; },
+	kw_by(node) { return node.text; },
+	kw_case(node) { return node.text; },
+	kw_catch(node) { return node.text; },
+	kw_collect(node) { return node.text; },
+	kw_compare(node) { return node.text; },
+	kw_context(node) { return node.text; },
+	kw_coordsys(node) { return node.text; },
+	kw_defaultAction(node) { return node.text; },
+	kw_do(node) { return node.text; },
+	kw_else(node) { return node.text; },
+	kw_exit(node) { return node.text; },
+	kw_for(node) { return node.text; },
+	kw_from(node) { return node.text; },
+	kw_function(node) { return node.text; },
+	kw_global(node) { return node.text; },
+	kw_group(node) { return node.text; },
+	kw_if(node) { return node.text; },
+	kw_in(node) { return node.text; },
+	kw_level(node) { return node.text; },
+	kw_local(node) { return node.text; },
+	kw_macroscript(node) { return node.text; },
+	kw_mapped(node) { return node.text; },
+	kw_menuitem(node) { return node.text; },
+	kw_not(node) { return node.text; },
+	kw_null(node) { return node.text; },
+	kw_objectset(node) { return node.text; },
+	kw_of(node) { return node.text; },
+	kw_on(node) { return node.text; },
+	kw_parameters(node) { return node.text; },
+	kw_persistent(node) { return node.text; },
+	kw_plugin(node) { return node.text; },
+	kw_rcmenu(node) { return node.text; },
+	kw_return(node) { return node.text; },
+	kw_rollout(node) { return node.text; },
+	kw_scope(node) { return node.text; },
+	kw_separator(node) { return node.text; },
+	kw_set(node) { return node.text; },
+	kw_struct(node) { return node.text; },
+	kw_submenu(node) { return node.text; },
+	kw_then(node) { return node.text; },
+	kw_time(node) { return node.text; },
+	kw_to(node) { return node.text; },
+	kw_tool(node) { return node.text; },
+	kw_try(node) { return node.text; },
+	kw_uicontrols(node) { return node.text; },
+	kw_undo(node) { return node.text; },
+	kw_utility(node) { return node.text; },
+	kw_when(node) { return node.text; },
+	kw_where(node) { return node.text; },
+	kw_while(node) { return node.text; },
+	kw_with(node) { return node.text; },
 };
 const visitorPatterns = {
 	// TOKENS
 	...tokensValue,
 	// LITERALS
-	Literal    (node, stack) { return stack.value;},
-	Identifier (node, stack) { return stack.value;},
-	Parameter (node, stack) { return stack.value;},
+	Literal(node, stack) { return stack.value; },
+	Identifier(node, stack) { return stack.value; },
+	Parameter(node, stack) { return stack.value; },
 
-	BitRange   (node, stack) { return `${stack.start}..${stack.end}`;},
-	// Declaration - DEPRECATED
+	BitRange(node, stack) { return `${stack.start}..${stack.end}`; },
+	// Declaration
 	Declaration(node, stack) {
-		return stack.value ? `${stack.id}=${stack.value}` : stack.id;
+		return stack.value ? stack.value : stack.id;
 	},
 	// Types
 	ObjectArray(node, stack) {
@@ -215,63 +215,118 @@ const visitorPatterns = {
 		}
 		return `struct ${stack.id}(${body})`;
 	},
-	StructScope(node, stack) {return stack.value;},
+	StructScope(node, stack) { return stack.value; },
 	// Functions
 	Function(node, stack) {
 		let decl = `${node.mapped ? 'mapped ' : ''}${stack.keyword} ${stack.id}`;
 		let args = ('args' in stack) ? joinStatements(stack.args) : '';
-		let params = ('params' in stack) ? joinStatements(stack.params) : '';
-		let body = exprTerm(stack.body);
-		return joinStatements([decl, args, params, '=', body]);
+
+		if ('params' in stack) {
+			let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+			return joinStatements([decl, args, params, '=', exprTerm(stack.body)]);
+		} else {
+			return joinStatements([decl, args,'=', exprTerm(stack.body)]);
+		}
 	},
 	FunctionReturn(node, stack) {
 		return joinStatements(['return', exprTerm(stack.body)]);
 	},
 	// Plugin
 	EntityPlugin(node, stack) {
-		let body = exprTerm(stack.body);
-		return joinStatements(['plugin', stack.superclass, stack.class, ...stack.params, '(', body, ')']);
+		let body = exprTerm(stack.body);		
+		let params = Array.isArray(stack.params) ? stack.params : [stack.params];
+		if (params) {
+			return joinStatements(['plugin', stack.superclass, stack.class, ...params, '(', body, ')']);
+		} else {
+			return joinStatements(['plugin', stack.superclass, stack.class, '(', body, ')']);
+		}
 	},
 	EntityPlugin_params(node, stack) {
 		let body = exprTerm(stack.body);
-		return `parameters ${stack.id} ${joinStatements(stack.params)}(${body})`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `parameters ${stack.id} ${params}(${body})`;
+		} else {
+			return `parameters ${stack.id}(${body})`;
+		}
 	},
 	PluginParam(node, stack) {
-		return `${stack.id} ${joinStatements(stack.params)}`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `${stack.id} ${params}`;
+		} else {
+			return stack.id;
+		}
 	},
 	// Tool
 	EntityTool(node, stack) {
-		return `tool ${stack.id} ${joinStatements(stack.params)}(${exprTerm(stack.body)})`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `tool ${stack.id} ${params}(${exprTerm(stack.body)})`;
+		} else {
+			return `tool ${stack.id}(${exprTerm(stack.body)})`;
+		}
 	},
 	// MacroScript
 	EntityMacroscript(node, stack) {
-		return `macroScript ${stack.id} ${joinStatements(stack.params)}(${exprTerm(stack.body)})`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `macroScript ${stack.id} ${params}(${exprTerm(stack.body)})`;
+		} else {
+			return `macroScript ${stack.id}(${exprTerm(stack.body)})`;
+		}
 	},
 	// rcMenu
 	EntityRcmenu(node, stack) {
 		return `rcmenu ${stack.id}(${exprTerm(stack.body)})`;
 	},
 	EntityRcmenu_submenu(node, stack) {
-		return `subMenu${stack.label}${stack.params}(${exprTerm(stack.body)})`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `subMenu${stack.label}${params}(${exprTerm(stack.body)})`;
+		} else {
+			return `subMenu${stack.label}(${exprTerm(stack.body)})`;
+		}
 	},
 	EntityRcmenu_menuitem(node, stack) {
-		return `menuItem ${stack.id}${stack.label}${joinStatements(stack.params)}`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		return `menuItem ${stack.id}${stack.label}${params}`;
 	},
 	EntityRcmenu_separator(node, stack) {
-		return joinStatements(['separator', stack.id, ...stack.params]);
+		let params = Array.isArray(stack.params) ? stack.params : [stack.params];
+		if (params) {
+			return joinStatements(['separator', stack.id, ...params]);
+		} else {
+			return joinStatements(['separator', stack.id]);
+		}
 	},
 	// Utility - Rollout
 	EntityUtility(node, stack) {
-		return `utility ${stack.id}${stack.title}${joinStatements(stack.params)}(${exprTerm(stack.body)})`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `utility ${stack.id}${stack.title}${params}(${exprTerm(stack.body)})`;
+		} else {
+			return `utility ${stack.id}${stack.title}(${exprTerm(stack.body)})`;
+		}
 	},
 	EntityRollout(node, stack) {
-		return `rollout ${stack.id}${stack.title}${joinStatements(stack.params)}(${exprTerm(stack.body)})`;
+		let params = Array.isArray(stack.params) ? joinStatements(stack.params) : stack.params;
+		if (params) {
+			return `rollout ${stack.id}${stack.title}${params}(${exprTerm(stack.body)})`;
+		} else {
+			return `rollout ${stack.id}${stack.title}(${exprTerm(stack.body)})`;
+		}
 	},
 	EntityRolloutGroup(node, stack) {
 		return `group${stack.id}(${exprTerm(stack.body)})`;
 	},
 	EntityRolloutControl(node, stack) {
-		return joinStatements([stack.class, stack.id, stack.text, ...stack.params]);
+		let params = Array.isArray(stack.params) ? stack.params : [stack.params];
+		if (params) {
+			return joinStatements([stack.class, stack.id, stack.text, ...params]);
+		} else {
+			return joinStatements([stack.class, stack.id, stack.text]);
+		}
 	},
 	// Event
 	Event(node, stack) {
@@ -310,15 +365,15 @@ const visitorPatterns = {
 			return `${left}${spaceAlphaNum(stack.operator)}${right}`;
 		} else {
 			let space =
-			/[-]$/gmi.test(stack.operator)
-				&& /^[-]/gmi.test(right)
-				? ' ' : '';
-			
+				/[-]$/gmi.test(stack.operator)
+					&& /^[-]/gmi.test(right)
+					? ' ' : '';
+
 			return `${left}${stack.operator}${space}${right}`;
 		}
 	},
-	LogicalExpression(node, stack) {return binaryNode(stack);},
-	UnaryExpression(node, stack) {return `${stack.operator}${stack.right}`;},
+	LogicalExpression(node, stack) { return binaryNode(stack); },
+	UnaryExpression(node, stack) { return `${stack.operator}${stack.right}`; },
 	// STATEMENTS
 	IfStatement(node, stack) {
 		let test = stack.test;
@@ -442,7 +497,8 @@ function visit(node, callbackMap) {
 			res = node;
 		}
 		else if (Array.isArray(node)) {
-			res = keys.map(x => stack[x]).join(';');
+			// res = keys.map(x => stack[x]).join(';');
+			res = keys.map(x => stack[x]).filter(e => e != null || e !== '').join('');
 		}
 		//--------------------------------
 		return res;
