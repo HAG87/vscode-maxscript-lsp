f91ec16b76b1d2021178f7f8abba591589ceb303
Atelier Bump <ariel.g87@gmail.com>
2020-12-25 15:29:13 -0600

    Fixes for Range and word search

--- server/src/mxsCompletions.ts
+++ server/src/mxsCompletions.ts
@@ -12,8 +12,6 @@ import { mxClassMembers } from './schema/mxsSchema-clases';
 import { mxInterfaceMembers } from './schema/mxsSchema-interfaces';
 import { mxStructsMembers } from './schema/mxsSchema-structs';
 //------------------------------------------------------------------------------------------
-/** A static collection of completion items*/
-const mxCompletions = new Array<CompletionItem>(...maxCompletions);
 const dotPattern = /([A-Za-z_][A-Za-z0-9_]+)[.]$/mi;
 
 /**
@@ -24,33 +22,38 @@ const dotPattern = /([A-Za-z_][A-Za-z0-9_]+)[.]$/mi;
 export function provideCompletionItems(document: TextDocument, position: Position): CompletionItem[]
 {
 
-	let lineTillCurrentPosition =
-		document.getText(Range.create(position.line, -1, position.line, position.character));
+	const lineTillCurrentPosition =
+		document.getText(Range.create(position.line, 0, position.line, position.character));
 
 	// escape strings - NOT WORKING RIGHT
 	// if (!(util.isPositionInString(lineTillCurrentPosition))) {
 	// 	return [];
 	// }
 
-	let termMatch = dotPattern.exec(lineTillCurrentPosition);
 	let result: CompletionItem[] = [];
 
+	const termMatch = dotPattern.exec(lineTillCurrentPosition);
 	if (termMatch) {
-		let theFoundItem = mxCompletions.find(item => item.label === termMatch![1]);
-		switch (theFoundItem?.kind) {
-			case CompletionItemKind.Class:
-				result = mxClassMembers?.[theFoundItem.label];
-				break;
-			case CompletionItemKind.Struct:
-				result = mxStructsMembers?.[theFoundItem.label];
-				break;
-			case CompletionItemKind.Interface:
-				result = mxInterfaceMembers?.[theFoundItem.label];
-				break;
-		}
+		// return properties, methods...
+		maxCompletions.forEach(item => {
+			if (item.label === termMatch![1]) {
+				switch (item.kind) {
+					case CompletionItemKind.Class:
+						result = mxClassMembers?.[item.label];
+						break;
+					case CompletionItemKind.Struct:
+						result = mxStructsMembers?.[item.label];
+						break;
+					case CompletionItemKind.Interface:
+						result = mxInterfaceMembers?.[item.label];
+						break;
+				}
+				return;
+			}
+		});
 	} else {
-		// Im not sure about this....
-		result = mxCompletions;
+		// return complete list of completions
+		result = maxCompletions;
 	}
 	return result;
 }
