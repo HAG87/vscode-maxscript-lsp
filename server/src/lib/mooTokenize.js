7d55c97a7d56651996c6a2c8291cf08a61adb690
Bump-MSI <atelierbump@gmail.com>
2024-05-28 13:33:52 -0600

    Fix: unicode support

--- server/src/lib/mooTokenize.js
+++ server/src/lib/mooTokenize.js
@@ -6,92 +6,113 @@ const { keywordsTypeDB } = require('./keywordsDB');
 // Moo Lexer
 const mxLexer = moo.compile({
 	// the comments
-	comment_SL: /--.*$/,
-	comment_BLK: { match: /\/\*(?:.|[\n\r])*?\*\//, lineBreaks: true },
+	comment_SL: /--.*$/u,
+	comment_BLK: { match: /\/\*(?:.|[\n\r])*?\*\//u, lineBreaks: true },
 
 	// strings
 	string: [
-		{ match: /@"(?:\\"|[^"])*?(?:"|\\")/, lineBreaks: true },
-		{ match: /"(?:\\["\\rntsx]|[^"])*?"/, lineBreaks: true },
+		{ match: /@"(?:\\"|[^"])*?(?:"|\\")/u, lineBreaks: true },
+		{ match: /"(?:\\["\\rntsx]|[^"])*?"/u, lineBreaks: true },
 		// { match: /"""[^]*?"""/, lineBreaks: true, value: x => x.slice(3, -3)},
 	],
 
 	// whitespace -  also matches line continuations
 	ws: [
-		{ match: /\\(?:[ \t]*[;\r\n]+)/, lineBreaks: true },
-		{ match: /[ \t]+/ }
+		{ match: /\\(?:[ \t]*[;\r\n]+)/u, lineBreaks: true },
+		{ match: /[ \t]+/u }
 	],
 
 	// newline: { match: /(?:[\r\n]|[\\]\s*[\r\n])+/, lineBreaks: true },
-	newline: { match: /(?:[;\r\n]+)/, lineBreaks: true },
+	newline: { match: /(?:[;\r\n]+)/u, lineBreaks: true },
 
 	// strings ~RESOURCE~
-	locale: /~[A-Za-z0-9_]+~/,
+	locale: /~[_\p{L}]+~/u,
 
 	path: [
-		{ match: /\$(?:(?:[A-Za-z0-9_*\\?]+|[.]{3}|'[^']+')[\\/]?)+/, lineBreaks: true },
-		{ match: /\$/ }
+		{ match: /\$(?:(?:[\p{L}0-9_*\\?]+|[.]{3}|'[^']+')[\\/]?)+/u, lineBreaks: true },
+		{ match: /\$/u }
 	],
 
 	// #name literals
 	name: [
-		{ match: /#[A-Za-z0-9_]+/ },
-		{ match: /#'[A-Za-z0-9_]+'/ }
+		{ match: /#[\p{L}0-9_]+/u },
+		{ match: /#'[\p{L}0-9_]+'/u }
 	],
 
 	identity: [
-		{ match: /'(?:(?:[^']|[\r\n])+)'/, lineBreaks: true },
-		{ match: /::[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/ },
-		{ match: /[&][A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/ },
+		{ match: /'(?:(?:[^']|[\r\n])+)'/u, lineBreaks: true },
+		{ match: /::[_\p{L}][0-9_\p{L}]*/u },
+		{ match: /[&][_\p{L}][0-9_\p{L}]*/u },
 		{
-			match: /[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
+			match: /[_\p{L}][0-9_\p{L}]*/u,
 			type: caseInsensitiveKeywords(keywordsTypeDB)
 		},
 	],
 
-	sharp: '#',
-	questionmark: '?',
+	sharp: /#/u,
+	questionmark: /\?/u,
 
 	// PARENS, BRACKETS, BRACES
-	lparen: '(',
-	rparen: ')',
-	lbracket: '[',
-	rbracket: ']',
-	lbrace: '{',
-	rbrace: '}',
+	lparen:   /\(/u,
+	rparen:   /\)/u,
+	lbracket: /\[/u,
+	rbracket: /\]/u,
+	lbrace:   /\{/u,
+	rbrace:   /\}/u,
 
 	// Operators.
-	comparison: ['==', '!=', '>', '<', '>=', '<='],
-	assign: ['=', '+=', '-=', '*=', '/='],
-	math: ['+', '-', '*', '/', '^'],
+	comparison:
+		[
+			/==/u,
+			/!=/u,
+			/>/u,
+			/</u,
+			/>=/u,
+			/<=/u
+		],
+	assign:
+		[
+			/[=]/u,
+			/\+[=]/u,
+			/-[=]/u,
+			/\*[=]/u,
+			/\/[=]/u
+		],
+	math:
+		[
+			/[+]/u,
+			/[-]/u,
+			/[*]/u,
+			/[/]/u,
+			/[\^]/u
+		],
 
 	// time format
 	time: [
-		{ match: /(?:(?:[0-9]+[.])?[0-9]+[msftMSFT])+/ },
-		{ match: /(?:(?:[0-9]+[.])[0-9]*[msftMSFT])+/ },
-		{ match: /[0-9]+[:][0-9]+[.][0-9]*/ }
+		{ match: /(?:(?:[0-9]+[.])?[0-9]+[msftMSFT])+/u },
+		{ match: /(?:(?:[0-9]+[.])[0-9]*[msftMSFT])+/u },
+		{ match: /[0-9]+[:][0-9]+[.][0-9]*/u }
 	],
 
 	// number formats
 	number: [
-		/0[xX][0-9a-fA-F]+/,
-		/(?:[0-9]*)[.](?:[0-9]+(?:[eEdD][+-]?[0-9]+)?)/, // 123.123d-6
-		/(?:[0-9]+\.(?!\.))/, // 123.
-		/[0-9]+(?:[lLpP]|[eEdD][+-]?[0-9]+)?/, // 456 | 123e-5 | integers
-		/(?:(?<!\.)\.[0-9]+(?:[eEdD][+-]?[0-9]+)?)/ // -.789e-9
+		/0[xX][0-9a-fA-F]+/u,
+		/(?:[0-9]*)[.](?:[0-9]+(?:[eEdD][+-]?[0-9]+)?)/u, // 123.123d-6
+		/(?:[0-9]+\.(?!\.))/u, // 123.
+		/[0-9]+(?:[lLpP]|[eEdD][+-]?[0-9]+)?/u, // 456 | 123e-5 | integers
+		/(?:(?<!\.)\.[0-9]+(?:[eEdD][+-]?[0-9]+)?)/u // -.789e-9
 	],
 
 	// DELIMITERS
-	bitrange: '..',
-	dot: '.',
-	comma: ',',
+	bitrange: /\.\./u,
+	dot:      /\./u,
+	comma:    /,/u,
 	// statement: ';',
-	colon: ':',
+	colon: /:/u,
 
 	// [\$?`] COMPLETE WITH UNWANTED CHARS HERE THAT CAN BREAK THE TOKENIZER
 	error: [
-		{ match: /[¿¡!`´]/, error: true },
-		{ match: /[/?\\]{2,}/ },
+		{ match: /[¿¡!`´]|[/?\\]{2,}/u, error: true },
 		// { match: /.+/ } //match any character left
 	],
 	// This contains the rest of the stack in case of error.
