23c8caf66f1c5042e520059eaac803a57f549f86
Bump-MSI <atelierbump@gmail.com>
2024-07-28 13:41:01 -0600

    Diagnostics: avoid duplicate entries Caused by two-stage parsing

--- src/Diagnostics.ts
+++ src/Diagnostics.ts
@@ -4,26 +4,38 @@
 import { Diagnostic, DiagnosticSeverity } from "vscode";
 import { DiagnosticType, IDiagnosticEntry } from "./types.js";
 import { Utilities } from "./utils.js";
-
+/*
 const diagnosticMap = new Map<DiagnosticType, DiagnosticSeverity>([
     [DiagnosticType.Hint, DiagnosticSeverity.Hint],
     [DiagnosticType.Info, DiagnosticSeverity.Information],
     [DiagnosticType.Warning, DiagnosticSeverity.Warning],
     [DiagnosticType.Error, DiagnosticSeverity.Error],
 ]);
-
+*/
 const diagnosticTypeMap = new Map<DiagnosticType, DiagnosticSeverity>();
 
 export function diagnosticAdapter(entries: IDiagnosticEntry[]): Diagnostic[]
 {
     const diagnostics: Diagnostic[] = [];
+    //  Avoid duplicate entries
+    let tempDiagnostic: IDiagnosticEntry | null = null;
+
     for (const entry of entries) {
+        if (tempDiagnostic) {
+            if (JSON.stringify(entry) === JSON.stringify(tempDiagnostic)) {
+                tempDiagnostic = entry;
+                continue;
+            }
+        }
+
         const diagnostic = new Diagnostic(
             Utilities.lexicalRangeToRange(entry.range),
             entry.message,
-            diagnosticTypeMap.get(entry.type)  
+            diagnosticTypeMap.get(entry.type)
         );
         diagnostics.push(diagnostic);
+
+        tempDiagnostic = entry;
     }
     return diagnostics;
 }
